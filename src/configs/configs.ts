2d11aba793b1e4f7fd0c20c0d8af87bd64a9556e
god-of-knowledge <w.m.qung@gmail.com>
2024-03-21 12:54:53 +0800

    configs

--- src/configs/configs.ts
+++ src/configs/configs.ts
@@ -1,9 +1,13 @@
 interface Config {
   saltRounds: number;
   jwtSecret: any;
+  databaseUrl: string;
 }
 
 export const configs: Config = {
   saltRounds: Number(process.env.SALT_ROUNDS) || 10,
   jwtSecret: process.env.JWT_SECRET || "hello",
+  databaseUrl:
+    process.env.DATABASE_URL ||
+    "postgresql://postgres:root@localhost:5432/personal_branding_db",
 };
